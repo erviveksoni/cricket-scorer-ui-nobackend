9e8800b7b6d3ad8c3f9fefd6ecf9da6a75daa94d
Bhavya Narula <bhavyanarula01@gmail.com>
2018-09-25 14:18:17 +0530

    eslint fix

--- src/ThisOver/ThisOver.js
+++ src/ThisOver/ThisOver.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Container from 'reactstrap/lib/Container';
 import Row from 'reactstrap/lib/Row';
 import Col from 'reactstrap/lib/Col';
 import './ThisOver.css';
 
+
 class ThisOver extends Component {
   render() {
-    const getBowlerName = function getBowlerName(bowlerId, playersArr) {
-      for (let i = 0; i < playersArr.length; i += 1) {
-        if (bowlerId === playersArr[i].id) {
-          return playersArr[i].name;
+    const getBowlerName = function getBowlerName(bowlerId, players) {
+      for (let i = 0; i < players.length; i += 1) {
+        if (bowlerId === players[i].id) {
+          return players[i].name;
         }
       }
       return '';
@@ -32,6 +34,12 @@ const mapStateAsProps = state => ({
   currentBowlerId: state.thisOver.currentBowlerId,
   playersArr: state.thisOver.players,
 });
+
+ThisOver.propTypes = {
+  currentBowlerId: PropTypes.number.isRequired,
+  playersArr: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
 
 export default connect(mapStateAsProps)(ThisOver);
 
