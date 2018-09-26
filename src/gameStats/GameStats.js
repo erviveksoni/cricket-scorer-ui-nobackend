fb1fba4d482498c70c9baeb30dee80af5f149c68
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2018-09-26 12:59:01 +0530

    #008: Fix lint warnings
    Co-authored-by: Prateek Arora <prk0027@gmail.com>

--- src/gameStats/GameStats.js
+++ src/gameStats/GameStats.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Container from 'reactstrap/lib/Container';
 import CurrentScoreHeading from '../scoreHeader/CurrentScoreHeading';
-import '../home/Home.css'
+import '../home/Home.css';
+
 const GameStats = () => (
   <Container className="page-width">
     <h3 className="text-align-center">GAME STATS</h3>
