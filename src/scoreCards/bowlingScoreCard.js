80919768460ef3d29e259de6dc1172f71c64b628
Vivek Soni <viveksni@INviveksnis-MacBook-Pro.local>
2018-09-26 15:00:53 +0530

    Adding bowling score card

--- src/scoreCards/bowlingScoreCard.js
+++ src/scoreCards/bowlingScoreCard.js
@@ -1,44 +1,52 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import Row from 'reactstrap/lib/Row';
-import Col from 'reactstrap/lib/Col';
+import BootstrapTable from 'reactjs-bootstrap-table';
 
-const BowlingScoreCard = props =>
-  (
-    <div className="home-component" >
-      <br />
-      <Row>
-        <Col >
-          <Row>
-            <Col md="5" xs="4">
-              <b>Test</b>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <br />
-      <Row>
-        <Col >
-          <Row>
-            <Col md="5" xs="4" />
-            {props.bowlingTeamPlayers}
-            <Col sm="1" xs="2" />
-            <Col className="text-align-right">
-          Test
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+const BowlingScoreCard = (props) => {
+  const displayHeaders = true;
+
+  function mRenderer(row) {
+    if (row.id === props.currentBowlerId) {
+      return (<span className="bst-no-select">{row.name}*</span>);
+    }
+
+    return (<span className="bst-no-select">{row.name}</span>);
+  }
+
+  const columns = [
+    { name: 'name', display: 'Bowler', renderer: mRenderer },
+    { name: 'totalOversBowled', display: 'Overs' },
+    { name: 'madins', display: 'Maiden' },
+    { name: 'runs', display: 'Run' },
+    { name: 'wickets', display: 'Wickets' },
+  ];
+
+  return (
+    <div>
+      <div>
+        <h5>Bowling Team</h5>
+      </div>
+      <BootstrapTable
+        select="single"
+        columns={columns}
+        data={props.bowlingTeamPlayers}
+        headers={displayHeaders}
+        activeClass="active"
+      >
+        <div className="well">There are no items to show</div>
+      </BootstrapTable>
     </div>);
+};
 
 BowlingScoreCard.propTypes = {
   bowlingTeamPlayers: PropTypes.instanceOf(Object).isRequired,
+  currentBowlerId: PropTypes.number.isRequired,
 };
 
-
 const mapStateToProps = state => ({
-  bowlingTeamPlayers: state.bowlerScorerReducer.bowlingTeamPlayers,
+  bowlingTeamPlayers: state.bowlerScorer.bowlingTeamPlayers,
+  currentBowlerId: state.thisOver.currentBowlerId,
 });
 
 export default connect(mapStateToProps)(BowlingScoreCard);
