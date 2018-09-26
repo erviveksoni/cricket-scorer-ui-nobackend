e5c3a8a568cceae759bbe617572dad45b748e25b
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2018-09-26 17:38:26 +0530

    #008:Finalize UI according to feedback
    Co-authored-by: Prateek Arora <prk0027@gmail.com>

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Route, HashRouter, Link } from 'react-router-dom';
-import { Button } from 'reactstrap';
+import { Route, HashRouter, NavLink } from 'react-router-dom';
 import GameStats from '../gameStats/GameStats';
 import Home from '../home/Home';
 import './routes.css';
@@ -17,14 +16,10 @@ const AppRouter = () =>
     <HashRouter>
       <div className="inherit-height w-75 wrapper">
         <h3 className="header text-align-center">CRICKET SCORER</h3>
-        <ul className="nav-list">
-          <li>
-            <Button className="nav-btn" color="secondary" tag={Link} to={Routes.HOME} value="Game Scorer">Game Scorer</Button>
-          </li>
-          <li>
-            <Button className="nav-btn" color="secondary" tag={Link} to={Routes.GAME_STATS} value="Game Stats">Game Stats&nbsp;&nbsp;</Button>
-          </li>
-        </ul>
+        <div className="vertical-menu">
+          <NavLink className="nav-btn" exact to={Routes.HOME} value="Game Scorer">Game Scorer</NavLink>
+          <NavLink className="nav-btn" to={Routes.GAME_STATS} value="Game Stats">Game Stats&nbsp;&nbsp;</NavLink>
+        </div>
         <Route exact path={Routes.HOME} component={Home} />
         <Route path={Routes.GAME_STATS} component={GameStats} />
       </div>
--- src/scorer/scorer.js
+++ src/scorer/scorer.js
@@ -40,21 +40,21 @@ class Scorer extends Component {
         <Row>
           <Col className="text-center">
             <div>
-              <button className={this.state.activeRunButton === 'runbtn-0' ? 'runbtn active' : 'runbtn'} value="0" onClick={() => this.setState({ activeRunButton: 'runbtn-0' })}>0</button>
+              <Button className={this.state.activeRunButton === 'runbtn-0' ? 'runbtn active' : 'runbtn'} value="0" onClick={() => this.setState({ activeRunButton: 'runbtn-0' })}>0</Button>
               &nbsp;
-              <button className={this.state.activeRunButton === 'runbtn-1' ? 'runbtn active' : 'runbtn'} value="1" onClick={() => this.setState({ activeRunButton: 'runbtn-1' })}>1</button>
+              <Button className={this.state.activeRunButton === 'runbtn-1' ? 'runbtn active' : 'runbtn'} value="1" onClick={() => this.setState({ activeRunButton: 'runbtn-1' })}>1</Button>
               &nbsp;
-              <button className={this.state.activeRunButton === 'runbtn-2' ? 'runbtn active' : 'runbtn'} value="2" onClick={() => this.setState({ activeRunButton: 'runbtn-2' })}>2</button>
+              <Button className={this.state.activeRunButton === 'runbtn-2' ? 'runbtn active' : 'runbtn'} value="2" onClick={() => this.setState({ activeRunButton: 'runbtn-2' })}>2</Button>
               &nbsp;
-              <button className={this.state.activeRunButton === 'runbtn-3' ? 'runbtn active' : 'runbtn'} value="3" onClick={() => this.setState({ activeRunButton: 'runbtn-3' })}>3</button>
+              <Button className={this.state.activeRunButton === 'runbtn-3' ? 'runbtn active' : 'runbtn'} value="3" onClick={() => this.setState({ activeRunButton: 'runbtn-3' })}>3</Button>
               &nbsp;
-              <button className={this.state.activeRunButton === 'runbtn-4' ? 'runbtn active' : 'runbtn'} value="4" onClick={() => this.setState({ activeRunButton: 'runbtn-4' })}>4</button>
+              <Button className={this.state.activeRunButton === 'runbtn-4' ? 'runbtn active' : 'runbtn'} value="4" onClick={() => this.setState({ activeRunButton: 'runbtn-4' })}>4</Button>
               &nbsp;
-              <button className={this.state.activeRunButton === 'runbtn-5' ? 'runbtn active' : 'runbtn'} value="5" onClick={() => this.setState({ activeRunButton: 'runbtn-5' })}>5</button>
+              <Button className={this.state.activeRunButton === 'runbtn-5' ? 'runbtn active' : 'runbtn'} value="5" onClick={() => this.setState({ activeRunButton: 'runbtn-5' })}>5</Button>
               &nbsp;
-              <button className={this.state.activeRunButton === 'runbtn-6' ? 'runbtn active' : 'runbtn'} value="6" onClick={() => this.setState({ activeRunButton: 'runbtn-6' })}>6</button>
+              <Button className={this.state.activeRunButton === 'runbtn-6' ? 'runbtn active' : 'runbtn'} value="6" onClick={() => this.setState({ activeRunButton: 'runbtn-6' })}>6</Button>
               &nbsp;&nbsp;
-              <Button color="success" className="submitbtn" onClick={this.nextball}>Next Ball</Button>
+              <Button className="submitbtn" onClick={this.nextball}>Next Ball</Button>
             </div>
           </Col>
         </Row>
